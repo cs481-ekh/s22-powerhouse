c559a37b2604467db129470eec9b9682b995b929
YeOldeGent <jostleavell@u.boisestate.edu>
2022-02-12 16:08:36 -0700

    changed a bit in build

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-#The Linter
+#Create the Linter
 node jslint.mjs GHKSimulator/scripts/main.js
+if [ $? -eq 0 ]; then
+    return 0
+else
+    return -1
+fi
 
 #Build
-npm GHKSimulator/scripts/main.js
+# npm GHKSimulator/scripts/main.js
+
 #exit 127
