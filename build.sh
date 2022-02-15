0f4ea6095bdca4a85f5597a3ef5f52291428725b
TBrooksie3 <t.brooksie@hotmail.com>
2022-02-14 17:13:15 -0700

    added sudo to eslint

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 #Create the Linter
 sudo apt install nodejs
-npm install
-npm install --save-dev jest
-npm i --save-dev eslint
+sudo npm install
+sudo npm install --save-dev jest
+sudo npm i --save-dev eslint
 npx eslint ./GHKSimulator/scripts/main.js
 if [ $? -eq 0 ]; then
     echo "Linter Passed"
