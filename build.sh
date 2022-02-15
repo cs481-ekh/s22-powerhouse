6f578c2c7ec8e65c8daacb53f0ec96e3969a9f2b
TBrooksie3 <63577508+TBrooksie3@users.noreply.github.com>
2022-02-15 15:28:39 -0700

    add test for each command

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #Create the Linter
-sudo apt install nodejs
-npm install
-curl -L https://www.jslint.com/jslint.mjs > jslint.mjs
-node jslint.mjs GHKSimulator/scripts/main.js
+sudo apt install nodejs || ( echo "Linter Failed"; exit 1; )
+npm install || ( echo "Linter Failed"; exit 1; )
+curl -L https://www.jslint.com/jslint.mjs > jslint.mjs || ( echo "Linter Failed"; exit 1; )
+node jslint.mjs GHKSimulator/scripts/main.js || ( echo "Linter Failed"; exit 1; )
+npm install --save-dev jest || ( echo "Linter Failed"; exit 1; )
 if [ $? -eq 0 ]; then
     echo "Linter Passed"
     exit 0
