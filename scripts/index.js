a446572b0714f51c36c333ec0baed76f19e79260
dawsonbiersdorff <70453382+dawsonbiersdorff@users.noreply.github.com>
2022-02-25 17:12:39 -0700

    Linked html input, created function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,46 @@
-
 /*jslint node: true*/
 function sum(a, b) {
     return a + b;
 }
 // module.exports = sum;
+
+//Converts give celcius temperature to Kelvin
+function converToKelvin(celcius) {
+    let ret = celcius + 273.15;
+    return ret;
+}
+
+//Imports values from html form and uses them to calculate Goldman-Hodgkin-Katz solution
+function myFunc() {
+
+    //Constants for the equation
+    let r = 8.3145;
+    let f = 96485.3415;
+
+    //Assigns values for variables from html form
+    let potasPerm = parseInt(document.getElementById("permeabilityTextbox1").value);
+    let potassium1 = parseInt(document.getElementById("concentrationTextbox1").value);
+    let potassium2 = parseInt(document.getElementById("concentrationTextbox2").value);
+    let sodiumPerm = parseInt(document.getElementById("permeabilityTextbox2").value);
+    let sodium1 = parseInt(document.getElementById("concentrationTextbox3").value);
+    let sodium2 = parseInt(document.getElementById("concentrationTextbox4").value);
+    let chloridePerm = parseInt(document.getElementById("permeabilityTextbox3").value);
+    let chloride1 = parseInt(document.getElementById("concentrationTextbox5").value);
+    let chloride2 = parseInt(document.getElementById("concentrationTextbox6").value);
+    let temp = parseInt(document.getElementById("temperature").value);
+    let kelvinTemp = converToKelvin(temp);
+
+    //Calculates value using our Goldman equation
+    let numerator = ((potasPerm * potassium2) + (sodiumPerm * sodium2) + (chloridePerm * chloride1));
+    let denominator = ((potasPerm * potassium1) + (sodiumPerm * sodium1) + (chloridePerm * chloride2))
+    let firstPart = (-(r*kelvinTemp) / f);
+    let secondPart = Math.log(numerator / denominator);
+    let num = firstPart * secondPart * 1000;
+    let ret = num.toFixed(1);
+
+    //Assigns it to our output value in html file
+    document.getElementById("output").innerHTML = ret;
+}
 
 
 $(document).ready(function(){
