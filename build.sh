c9a61347892a83f7b30bf7cabfcbbd6d24a28cc8
TBrooksie3 <t.brooksie@hotmail.com>
2022-02-27 22:25:26 -0700

    change path on linter

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@
 sudo apt install nodejs || ( echo "Install NodeJS Failed"; exit 1; )
 npm install || ( echo "NPM Install Failed"; exit 1; )
 curl -L https://www.jslint.com/jslint.mjs > jslint.mjs || ( echo "Download Linter Failed"; exit 1; )
-node jslint.mjs GHKSimulator/scripts/main.js || ( echo "Linting Failed"; exit 1; )
+node jslint.mjs ./scripts/index.js || ( echo "Linting Failed"; exit 1; )
 npm install --save-dev jest || ( echo "Jest Install Failed"; exit 1; )
-echo "Build Passed"
-exit 0
+if [ $? -eq 0 ]; then
+    echo "Build Passed"
+    exit 0
+else
+    echo "Build Failed"
+    exit -1
+fi
