967bb1bdca0c68bf7709f6667ce5ef823b2ba365
TBrooksie3 <t.brooksie@hotmail.com>
2022-02-27 22:21:36 -0700

    fix build script

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 #Create the Linter
-sudo apt install nodejs || ( echo "Linter Failed"; exit 1; )
-npm install || ( echo "Linter Failed"; exit 1; )
-curl -L https://www.jslint.com/jslint.mjs > jslint.mjs || ( echo "Linter Failed"; exit 1; )
-node jslint.mjs GHKSimulator/scripts/main.js || ( echo "Linter Failed"; exit 1; )
-npm install --save-dev jest || ( echo "Linter Failed"; exit 1; )
-if [ $? -eq 0 ]; then
-    echo "Linter Passed"
-    exit 0
-else
-    echo "Linter Failed"
-    exit -1
-fi
+sudo apt install nodejs || ( echo "Install NodeJS Failed"; exit 1; )
+npm install || ( echo "NPM Install Failed"; exit 1; )
+curl -L https://www.jslint.com/jslint.mjs > jslint.mjs || ( echo "Download Linter Failed"; exit 1; )
+node jslint.mjs GHKSimulator/scripts/main.js || ( echo "Linting Failed"; exit 1; )
+npm install --save-dev jest || ( echo "Jest Install Failed"; exit 1; )
+echo "Build Passed"
+exit 0
